7a15b4f397b72dd3a8022538cdfc03a09507c3e9
artpaes <artpaes@gmail.com>
2016-02-09 17:35:23 +0100

    adding tooltip/ mouseover

--- js/basic_membership_map.js
+++ js/basic_membership_map.js
@@ -5,9 +5,18 @@ var width = 800,
 
 var projection = d3.geo.mercator()
     .center([24, 52])
-    .scale(700)
+    .scale(500)
     .rotate([15, 0])
     .translate([width / 2, height / 2]);
+
+
+var tooltip = d3.select("body")
+    .append("div")
+    .attr("class","tooltip")
+    .style("opacity", 0);
+    //.style("z-index", "10")
+    //.style("visibility", "hidden")
+    //.text("a simple tooltip");
 
 var color = d3.scale.ordinal()
     .domain(["", "EEA/Other", "EU Non-SSM", "SSM"])
@@ -19,7 +28,11 @@ var path = d3.geo.path()
 var svgMap = d3.select("#basic_map")
     .append("svg")
     .attr("width", width)
-    .attr("height", height);
+    .attr("height", height)
+    //.on("mouseover", function(){return tooltip.style("visibility", "visible");})
+    //.on("mousemove", function(){return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
+    //.on("mouseout", function(){return tooltip.style("visibility", "hidden");});
+
 
 var legend = d3.select('svg')
     .append("g")
@@ -53,7 +66,13 @@ d3.json("json/europeClean.json", function(error, europe) {
             rateById[d.id] = d.membership;
         });
 
-        var subunits = topojson.feature(europe,                               europe.objects.subunits);
+        var subunits = topojson.feature(europe, europe.objects.subunits);
+
+        // create a lookup object for adding banking union members data to each country
+        var lookup = {};
+        for (var i = 0, len = Membership.length; i < len; i++) {
+          lookup[Membership[i].id] = Membership[i];
+        };
 
         svgMap.append("path")
             .datum(subunits)
@@ -65,6 +84,17 @@ d3.json("json/europeClean.json", function(error, europe) {
             .attr("d", path)
             .style("fill", function(d) {
                 return color(rateById[d.id]);
-            });
+            })
+            .on("mouseover", function(d) {
+                console.log(lookup[d.id])
+                tooltip.transition()
+                  .duration(100)
+                  .style("opacity", .9);
+                tooltip.html(lookup[d.id].country)
+                  .style("left", (d3.event.pageX) + "px")
+                  .style("top", (d3.event.pageY) + "px")
+                  ;
+              });
+            
     });
 });
